b189db31b130349bb760c38b5bedfb88c33fb2b9
Сергей Лобанчук <slobanchuk@yandex.ru>
2023-05-04 11:46:31 +0300

    examples003_div_newRundom

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 18;
-double numberB = 5;
+﻿double numberA = new Random().Next(10, 20); // 10,11,12,13,14,15,16,17,18,19
+Console.WriteLine(numberA);
+double numberB = new Random().NextDouble();
+Console.WriteLine(numberB);
 double sum = numberA / numberB;
 Console.WriteLine(sum);

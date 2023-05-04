b31ba7f98b715bd6b6f8e2512ee8ed0a6a6d5169
Сергей Лобанчук <slobanchuk@yandex.ru>
2023-05-04 12:21:38 +0300

    example006_Bale

---
+++ Examples006_Bale/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 111;
+int b = 8;
+int c = 656;
+int d = 2;
+int e = 45;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("max = " + max);
+
+
